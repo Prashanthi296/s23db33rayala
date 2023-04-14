810c6ee8c97aa4004fbb80a468a87beacba7024a
Prashanthi296 <123036594+Prashanthi296@users.noreply.github.com>
2023-04-13 23:09:26 -0500

    Final Lab

--- controllers/cinema.js
+++ controllers/cinema.js
@@ -10,9 +10,16 @@ exports.cinema_list = async function (req, res) {
   }
 };
 // for a specific Cinema.
-exports.cinema_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Cinema detail: " + req.params.id);
-};
+exports.cinema_detail = async function(req, res) {
+  console.log("detail" + req.params.id)
+  try {
+  result = await Cinema.findById( req.params.id)
+  res.send(result)
+  } catch (error) {
+  res.status(500)
+  res.send(`{"error": document for id ${req.params.id} not found`);
+  }
+ };
 // Handle Cinema create on POST.
 exports.cinema_create_post = async function (req, res) {
   console.log(req.body);
@@ -38,9 +45,25 @@ exports.cinema_delete = function (req, res) {
   res.send("NOT IMPLEMENTED: Cinema delete DELETE " + req.params.id);
 };
 // Handle Cinema update form on PUT.
-exports.cinema_update_put = function (req, res) {
-  res.send("NOT IMPLEMENTED: Cinema update PUT" + req.params.id);
-};
+exports.cinema_update_put = async function(req, res) {
+  console.log(`update on id ${req.params.id} with body
+ ${JSON.stringify(req.body)}`)
+  try {
+  let toUpdate = await Cinema.findById( req.params.id)
+  // Do updates of properties
+  if(req.body.cinema_Name)
+  toUpdate.cinema_Name = req.body.cinema_Name;
+  if(req.body.cinema_genre) toUpdate.cinema_genre = req.body.cinema_genre;
+  if(req.body.cinema_parts) toUpdate.cinema_parts = req.body.cinema_parts;
+  let result = await toUpdate.save();
+  console.log("Sucess " + result)
+  res.send(result)
+  } catch (err) {
+  res.status(500)
+  res.send(`{"error": ${err}: Update for id ${req.params.id}
+ failed`);
+  }
+ };
 exports.cinema_view_all_Page = async function (req, res) {
   try {
     theCinemas = await Cinema.find();
